21f4aa552558ffe2448d11dca7a0b266edc4b1e3
numansaady <numansaady@gmail.com>
2022-11-14 20:34:33 +0600

    tours route and controller added

---
+++ controllers/tours.controller.js
@@ -0,0 +1,47 @@
+const Tour = require("../models/Tour");
+
+// Get All Tours
+exports.getAlltours = async (req, res, next) => {
+    try {
+        const { fields, sort } = req.query;
+    const filteredFields = fields?.split(",")?.join(" ");
+    console.log(filteredFields);
+    const limit = Number(req.query.limit) || 5;
+    const page = Number(req.query.page) || 0;
+    const tours = await Tour.find({})
+      .select(filteredFields)
+      .sort(sort)
+      .skip(page * limit)
+      .limit(limit);
+    res.status(200).send({ success: true, data: tours });
+    } catch (error) {
+        next(error);
+    }
+}
+
+// Insert A New Tour
+exports.createTour = async (req, res, next) => {
+    try {
+      const tours = await Tour.create(req.body);
+      res
+        .status(200)
+        .send({ success: true, message: "Tour Created Successfully" });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+// Get a specific Tour
+exports.getSpecificTour = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const tour = await Tour.findOneAndUpdate(
+        { _id: id },
+        { $inc: { views: 1 } }
+      );
+      res.status(200).send({ success: true, data: tour });
+    } catch (error) {
+      next(error);
+    }
+  };
+  
